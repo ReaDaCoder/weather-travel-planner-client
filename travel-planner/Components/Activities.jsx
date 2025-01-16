06037186585d18cd198f8723542c830d9638cec6
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2025-01-16 14:51:27 +0200

    Updated front

--- travel-planner/Components/Activities.jsx
+++ travel-planner/Components/Activities.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 export default function Activities(){
     let activities = [
-        ["Go hiking", "Have a picnic in the park", "Visit a beach or go swimming", "Explore a botanical garden or nature reserve"],
-        ["Take a leisurely walk or jog", "Visit an art gallery or museum","Read a book or journal in a peaceful park"],
-        ["Bake cookies or try out new recipes","Watch movies or binge a new TV series","Work on crafts like knitting, painting, or DIY projects"],
-        ["Go skiing, snowboarding, or snowshoeing", "Build a snowman or have a snowball fight","Capture snowy landscapes or wildlife"],
-        ["Fly a kite in an open field", "Surf or sail if near a suitable body of water","Visit a local coffee shop or library" ]
-    ];
+        { category: "Outdoor Activities", items: ["Go hiking", "Have a picnic in the park", "Visit a beach or go swimming", "Explore a botanical garden or nature reserve"] },
+        { category: "Leisure Activities", items: ["Take a leisurely walk or jog", "Visit an art gallery or museum", "Read a book or journal in a peaceful park"] },
+        { category: "Indoor Activities", items: ["Bake cookies or try out new recipes", "Watch movies or binge a new TV series", "Work on crafts like knitting, painting, or DIY projects"] },
+        { category: "Winter Activities", items: ["Go skiing, snowboarding, or snowshoeing", "Build a snowman or have a snowball fight", "Capture snowy landscapes or wildlife"] },
+        { category: "Spring Activities", items: ["Fly a kite in an open field", "Surf or sail if near a suitable body of water", "Visit a local coffee shop or library"] }
+      ];
+      
     return(
         <>
         <h1>Activities</h1>
